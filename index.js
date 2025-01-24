36082a39a6a5e85cb08f2598f4725a077efb6c03
Tajwinder Singh <tajwinder@Tajwinders-MBP.lan>
2025-01-24 12:38:07 +0530

    use es6 modules

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'https://esm.sh/react@18';
+import ReactDOM from 'https://esm.sh/react-dom@18';
+
+ReactDOM.render(
+  React.createElement('h1', null, 'My React Project with ES6 Modules'),
+  document.getElementById('root')
+);
